df63f2877a774a57a9f3b3abd3b18bdd5cb2565e
Tshaba Phomolo Benedict <benedicttshaba@gmail.com>
2017-08-27 12:37:27 +0200

    Create a test program

---
+++ test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "clist.h"
+
+int main() {
+	const char *names[] = {"Phomolo", "Thekgo", "Puleng", "Tebatso", "Molemo", "Tshepo", "Isaac"};
+	struct Table *t = create();
+	ID id;
+	for(int i=0;i<7;i++) {
+		add(t, S, names[i], &names[i]);
+		add(t, I, names[(i+3)%7], &i);
+	}
+	double pi = 3.1415;
+	add(t, D, "PIE", &pi);
+	print(t);
+	libr8(t);
+	return 0;
+}
