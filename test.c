0173dc53e9f2d94e48b60c746f3ce3ad62e9d286
Tshaba Phomolo Benedict <benedicttshaba@gmail.com>
2017-08-28 07:34:45 +0200

    Add test for pop function

--- test.c
+++ test.c
@@ -16,7 +16,14 @@ int main() {
 	printf("\npie is in list @ %d\nphomolo is in list @ %d\n",is_in(t,D,&pi), is_in(t,S,&names[0]));
 	const char *str[] = {"notinlist","test"};
 	printf("is_in(%s) returned: %d\nis_in(%s) returned: %d\n",str[0],is_in(t,S,&str[0]),str[1],is_in(t,S,&str[1]));
-
+	
+	int i = 4;
+	while(i>0){
+	pop(t);
+	i--;
+	}
+	printf("length of new list: %d\n",length(t));
+	print(t);
 	libr8(t);
 	return 0;
 }
