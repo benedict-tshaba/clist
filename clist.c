bb5db4a17d5f71287a68d028ee3ac4f3650d2a94
Tshaba Phomolo Benedict <benedicttshaba@gmail.com>
2017-08-30 15:50:01 +0200

    Add licensing header

--- clist.c
+++ clist.c
@@ -1,3 +1,23 @@
+/*
+# clist library.
+# A research program into the implemenation of a generic list type in C,
+# it serves as a proof of concept that Python's list object can be implemented
+# and used within a C program.
+# Copyright (C) 2017 Tshaba Phomolo Benedict
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
